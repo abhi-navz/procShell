4c2e75cef378b3535e005153a386a0c8ce7331a5
abhi-navz <abhinavg01441@gmail.com>
2026-06-06 17:38:05 +0530

    Implement exit command and tokenization

--- src/main.c
+++ src/main.c
@@ -1,16 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_INPUT 1024
+#define MAX_ARGS 100
 
 int main()
 {
-    char input[1024];
+    char input[MAX_INPUT];
+    char *args[MAX_ARGS];
 
     while(1)
     {
         printf("ProcShell> ");
 
-        fgets(input, sizeof(input), stdin);
+        if(fgets(input, sizeof(input), stdin) == NULL)
+        {
+            break;
+        }
 
-        printf("You entered: %s", input);
+        input[strcspn(input, "\n")] = '\0';
+
+        if(strcmp(input, "exit") == 0)
+        {
+            break;
+        }
+
+        int i = 0;
+
+        char *token = strtok(input, " ");
+
+        while(token != NULL)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        args[i] = NULL;
+
+        printf("Parsed Command:\n");
+
+        for(int j = 0; args[j] != NULL; j++)
+        {
+            printf("%s\n", args[j]);
+        }
     }
 
     return 0;
