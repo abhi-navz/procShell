3dcefbab4d3fb68a718f5beef4bcfd6331e6f337
abhi-navz <abhinavg01441@gmail.com>
2026-06-06 18:21:49 +0530

    Add command execution using fork and execvp

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 #define MAX_INPUT 1024
 #define MAX_ARGS 100
@@ -20,6 +23,11 @@ int main()
 
         input[strcspn(input, "\n")] = '\0';
 
+        if(strlen(input) == 0)
+        {
+            continue;
+        }
+
         if(strcmp(input, "exit") == 0)
         {
             break;
@@ -37,11 +45,22 @@ int main()
 
         args[i] = NULL;
 
-        printf("Parsed Command:\n");
+        pid_t pid = fork();
 
-        for(int j = 0; args[j] != NULL; j++)
+        if(pid == 0)
         {
-            printf("%s\n", args[j]);
+            execvp(args[0], args);
+
+            perror("execvp");
+            exit(1);
+        }
+        else if(pid > 0)
+        {
+            waitpid(pid, NULL, 0);
+        }
+        else
+        {
+            perror("fork");
         }
     }
 
